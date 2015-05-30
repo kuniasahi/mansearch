02bdf6f35a5b7f2b544b50f767d6e5fde2948f50
yuguoxu <yuguoxu@allwinnertech.com>
2015-05-30 15:04:28 +0800

    first init search tools support baidu tieba

---
+++ README.md
@@ -0,0 +1,6 @@
+a man private search tools in china net such as baidu tieba...
+
+一个猥琐的工具库，发帖纪录人肉搜索工具，计划支持贴吧，微博。。。腾讯空间、博客等，目前只支持贴吧
+
+用法 python tiebasearch.py -k xxx 
+xxx是贴吧ID
---
+++ tiebasearch.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import re
+import os
+import sys
+import time
+import random
+import urllib
+
+
+def get_urls_in_apage(page):
+   out = re.findall(r'href\=\"\/p\/[^\s]*',page)
+   for i in out:
+      #print i
+      t = "http://tieba.baidu.com" + i[6:len(i)-1]
+      print t
+      cid = re.findall(r'\#[^\s]*',t)
+      ids = cid[0][1:]
+      #print ids
+
+   
+
+def get_record_size(tname):
+   urls = "http://tieba.baidu.com/f/search/ures?ie=utf-8&kw=&qw=&rn=30&un="
+   req = urllib.urlopen(urlstr + tname)
+   web = req.read()
+   totalsizestr = re.findall(r's\_nav\_right hasPage[^\s]*',web)
+   totalsize = re.findall(r'[0-9][0-9]{0,}',totalsizestr[0])
+   sizex = int(totalsize[0])
+   return sizex
+
+if __name__ == '__main__':
+   urlstr = "http://tieba.baidu.com/f/search/ures?ie=utf-8&kw=&qw=&rn=30&un="
+   if(len(sys.argv)==3 and sys.argv[1] == '-k' ):
+       namex = sys.argv[2]
+       sizex = get_record_size(namex)
+       print "find all record size = " + str(sizex)
+       rn = 30
+       pn = sizex / rn
+       while pn > 0:
+          reqx = urllib.urlopen(urlstr + namex +"&" + "pn=" + str(pn))
+          web2 = reqx.read()
+          get_urls_in_apage(web2)
+          pn = pn - 1
+          print str(pn)
+          time.sleep(1)
